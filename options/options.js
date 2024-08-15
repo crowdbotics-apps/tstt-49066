09a03a617fcc00b48435a153e832adfcf2da6db6
Zlemo <zlemoangry@gmail.com>
2024-08-15 21:13:51 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tstt-49066",
-  url: "https://tstt-49066.botics.co",
-  api: "https://tstt-49066.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "tstt-49066",
+  "url": "https://tstt-49066.botics.co",
+  "api": "https://tstt-49066.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "about-the-app"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
